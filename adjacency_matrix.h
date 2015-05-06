753a2f1d5818f522e79f287e7e268cc4f26c2c98
daktyl <wencel.krzysztof@gmail.com>
2015-05-06 16:40:53 +0200

    Adjacency matrix with creation, DFS, BFS and printing functions.

---
+++ adjacency_matrix.h
@@ -0,0 +1,9 @@
+#ifndef GRAPHS_ADJACENCY_MATRIX_H
+#define GRAPHS_ADJACENCY_MATRIX_H
+
+void AdjMatrixCreate(int vertexCount);
+void AdjMatrixDFSSort(int vertex);
+void AdjMatrixBFSSort(int vertex);
+void AdjMatrixPrint();
+
+#endif //GRAPHS_ADJACENCY_MATRIX_H
---
+++ utils.h
@@ -0,0 +1,6 @@
+#ifndef GRAPHS_UTILS_H
+#define GRAPHS_UTILS_H
+
+int RandomBetween(int begin, int end);
+
+#endif //GRAPHS_UTILS_H
