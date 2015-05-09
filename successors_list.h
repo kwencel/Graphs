d9558c4707ed1038eda353cdba03827616d938bc
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2015-05-09 21:23:19 +0200

    Added SuccessorsList class.

--- successors_list.h
+++ successors_list.h
@@ -1,8 +1,22 @@
 #ifndef GRAPHS_SUCCESSORS_LIST_H
 #define GRAPHS_SUCCESSORS_LIST_H
 
-void SuccListCreate(int VertexCount);
-void SuccListPrint();
+#include <vector>
+#include <forward_list>
+using namespace std;
+
+class SuccessorsList {
+private:
+    int vertexCount;
+    vector<forward_list<int>> adjList;                // vector of lists
+    forward_list<int> succList;                       // list of successors
+    forward_list<int>::iterator currentVertex;        // pointer in list
+
+public:
+    SuccessorsList(int vertexCout = 6);
+
+    void print();
+};
 
 #endif //GRAPHS_SUCCESSORS_LIST_H
 
